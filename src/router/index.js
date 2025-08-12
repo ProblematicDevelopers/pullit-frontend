548d44e1182ac9bdc54d09bc4a1d52f9bf1257f7
sangjin oh <osj4040@gmail.com>
2025-08-12 15:26:17 +0900

    feat:report 구조 작업

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../Home.vue'
-import statistics from '../components/statistics/StatisticsTest.vue'
+import Report from '../components/student/report/BasicReport.vue'
 import StudentMain from '../components/student/Main.vue'
 import StudentLayout from '@/components/student/StudentLayout.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/statistics', name: 'statistics', component: statistics },
   {
     path: '/student',
     component: StudentLayout, // 공통 레이아웃 or StudentMain 같은 것
     children: [
       { path: 'main', name: 'student.main', component: StudentMain },
+      { path: 'report', name: 'student.report', component: Report},
       // { path: 'exam/:id', name: 'student.exam', component: StudentExam },
       // { path: 'result/:id', name: 'student.result', component: StudentResult },
     ],
