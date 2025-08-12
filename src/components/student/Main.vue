36c11a7b7417624ffa5ccc3c579e782338cfc92d
ddanggoos <hanck9595@gmail.com>
2025-08-12 14:07:46 +0900

    student page 추가

---
+++ src/components/student/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Student Main</h1>
+    <button class="btn btn-danger">dsd</button>
+    <div class="row">
+      <div class="col-3">123</div>
+      <div class="col-3">123</div>
+      <div class="col-3">123</div>
+      <div class="col-3">123</div>
+      <div class=""><button class="btn btn-danger">123</button></div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'StudentMain',
+}
+</script>
+
+<style scoped></style>
