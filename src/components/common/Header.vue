7ef90ad5020ec0514c4bbb35a8f3bc18145c2d8a
parksorry96 <pjs96525525@gmail.com>
2025-09-03 09:33:36 +0900

    feat: console log 제거

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -99,18 +99,18 @@
             </router-link>
 
 
-            <router-link
-              to="/exam-management"
-              class="nav-item"
-              :class="{ active: isCurrentRoute('/exam-management') }"
-            >
-              <svg viewBox="0 0 24 24" class="nav-icon">
-                <path
-                  d="M19 3H18V1H16V3H8V1H6V3H5C3.89 3 3.01 3.9 3.01 5L3 19C3 20.1 3.89 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3M19 19H5V8H19V19M7 10H12V15H7"
-                />
-              </svg>
-              <span>시험관리</span>
-            </router-link>
+<!--            <router-link-->
+<!--              to="/exam-management"-->
+<!--              class="nav-item"-->
+<!--              :class="{ active: isCurrentRoute('/exam-management') }"-->
+<!--            >-->
+<!--              <svg viewBox="0 0 24 24" class="nav-icon">-->
+<!--                <path-->
+<!--                  d="M19 3H18V1H16V3H8V1H6V3H5C3.89 3 3.01 3.9 3.01 5L3 19C3 20.1 3.89 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3M19 19H5V8H19V19M7 10H12V15H7"-->
+<!--                />-->
+<!--              </svg>-->
+<!--              <span>시험관리</span>-->
+<!--            </router-link>-->
 
 
             <router-link
